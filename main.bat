3b8d03372f27563b87a89bf83ea8130733f757ae
Carlos <55070485+git-mare@users.noreply.github.com>
2023-09-07 04:09:44 -0300

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,4 +1,4 @@
 @echo off
 set LOVE_PATH=C:\Program Files\LOVE
 
-"%LOVE_PATH%\love.exe" "C:\Users\marea\Documents\MyLove\myfarm"
+"%LOVE_PATH%\love.exe" "C:\Users\[USERNAME]\Documents\MyLove\myfarm"
